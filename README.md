a808e6fe5df68e66babe62775f385460bd294783
Marvin Gudel <marvin.gudel@gmail.com>
2020-05-08 12:24:27 +0200

    mqtt support

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LogMice
 Simple Python script to log movements of multiple mice present as Linux char-devices.
+
+
+## Publishing movements via MQTT
+MQTT can be configured to publish movements in defined intervalls.
+Messages are formatted in *JSON* and contain accumulated data for every mouse which moved.
--- logmice.py
+++ logmice.py
@@ -4,12 +4,21 @@ import time
 import signal
 import os
 import threading
+import paho.mqtt.client as mqtt
+import json
 
 # Config Variables
 ###########################
 
-PRINTDATA = True
+PRINTDATA = False
+LOGDATA = True
 LOGDIR = "logs"
+
+MQTT_ENABLE = True
+MQTT_HOST = "127.0.0.1"
+MQTT_PORT = 1883
+MQTT_TOPIC = "test/logmice"
+MQTT_MSG_INTERVAL = 0.5 # value in seconds
 
 ###########################
 
@@ -20,7 +29,10 @@ def ctrl_c(signal, frame):
     print("Stopping threads...")
     for thread in threads:
         thread.setStop()
-        thread.saveCSV()
+        if LOGDATA:
+            thread.saveCSV()
+    mqttHandler.setStop()
+    mqttHandler.join()
     exit()
 signal.signal(signal.SIGINT, ctrl_c)
 
@@ -36,6 +48,7 @@ if "mice.config" not in os.listdir("."):
 if LOGDIR not in os.listdir("."):
     os.mkdir(LOGDIR)
 
+# parse config and create file deskriptors for all devices
 inpDevices = open("mice.config", "r")
 mice = {}
 for dev in inpDevices:
@@ -51,8 +64,54 @@ if len(mice) == 0:
     print("\tuse \"ls -la /dev/input/by-path\" to list available mice")
     exit()
 
-#csv = open('output.csv', "w")
 
+# MQTT Setup
+######################################
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT Broker! status code:", rc)
+
+class mqttThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.mqttcl = mqtt.Client()
+        self.mqttcl.on_connect = on_connect
+        
+        self.mqttcl.connect(MQTT_HOST, MQTT_PORT, 60)
+        self.mqttcl.loop_start()
+    def run(self):
+        summed = {}
+        data = {}
+
+        while self.running:
+            for thread in threads:
+                data[thread.dev_name] = thread.getNewValues()
+            for mouse in data:
+                dx = dy = 0
+                if len(data[mouse]) == 0: # skip when nothing available
+                    if mouse in summed:
+                        del summed[mouse]
+                    continue
+                for update in data[mouse]: # sum up all movements
+                    dx += update[1]
+                    dy += update[2]
+                # append to dict
+                summed[mouse] = {"from_ms": data[mouse][0][0], "to_ms": data[mouse][-1][0], "dx": dx, "dy": dy}
+
+            if len(summed) > 0: # only send an update when data is available
+                self.mqttcl.publish(MQTT_TOPIC, json.dumps(summed))
+            time.sleep(MQTT_MSG_INTERVAL)
+
+        self.mqttcl.disconnect()
+
+
+    def setStop(self):
+        self.running = False
+# end of mqtt thread class
+
+
+# Thread implementation for Mouse reading
 ######################################
 
 def millis():
@@ -63,6 +122,8 @@ class mouseThread(threading.Thread):
         threading.Thread.__init__(self)
         self.dev_handle = dev_handle
         self.dev_name = dev_name
+        self.lock = threading.Lock()
+        self.cursor = 0
     def run(self):
         self.running = True
         self.readings = []
@@ -84,7 +145,8 @@ class mouseThread(threading.Thread):
                 
             dx = to_signed(dx)
             dy = to_signed(dy)
-            self.readings.append((now, dx, dy))
+            with self.lock:
+                self.readings.append((now, dx, dy))
             if PRINTDATA:
                 print(self.dev_name, dx, dy)
         print("thread for", self.dev_name, "stopped")
@@ -97,14 +159,25 @@ class mouseThread(threading.Thread):
             print("\nNo data to save for ", self.dev_name, "!\n")
             return
         with open(LOGDIR+"/"+self.dev_name+".csv", 'w') as csv:
-            for reading in self.readings:
-                csv.write(str(reading[0])+','+str(reading[1])+','+str(reading[2])+'\n')
+            with self.lock:
+                for reading in self.readings:
+                    csv.write(str(reading[0])+','+str(reading[1])+','+str(reading[2])+'\n')
         print("\nSaved csv for ", self.dev_name,"\n")
         
+    def getNewValues(self):
+        with self.lock:
+            current = self.cursor
+            self.cursor = len(self.readings)
+            return self.readings[current:]
+# end of mouse thread class
 
-# create and start a thread for every mice
+# create and start a thread for every mouse
 for key in mice.keys():
     threads.append(mouseThread(mice[key], key))
 for thread in threads:
     thread.start()
+
+if MQTT_ENABLE:
+    mqttHandler = mqttThread()
+    mqttHandler.start()
 print("Multi-reader started (reading", len(mice), "devices, debug output",PRINTDATA,")")
